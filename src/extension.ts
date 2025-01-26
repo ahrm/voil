6e510982cd93e86aa6d34f023742f225a0fd70a9
ahrm <a.hr.mostafavi@gmail.com>
2025-01-26 20:23:22 +0330

    Improve gitignore handling

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,141 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 
+// taken from https://github.com/fitzgen/glob-to-regexp/blob/master/index.js
+let globToRegex = (glob: string) => {
+    if (glob.endsWith("/")){
+        glob = glob + "**";
+    }
+
+    if (typeof glob !== 'string') {
+        throw new TypeError('Expected a string');
+    }
+
+    var str = String(glob);
+
+    // The regexp we are building, as a string.
+    var reStr = "";
+
+    // Whether we are matching so called "extended" globs (like bash) and should
+    // support single character matching, matching ranges of characters, group
+    // matching, etc.
+    var extended = true;
+
+    // When globstar is _false_ (default), '/foo/*' is translated a regexp like
+    // '^\/foo\/.*$' which will match any string beginning with '/foo/'
+    // When globstar is _true_, '/foo/*' is translated to regexp like
+    // '^\/foo\/[^/]*$' which will match any string beginning with '/foo/' BUT
+    // which does not have a '/' to the right of it.
+    // E.g. with '/foo/*' these will match: '/foo/bar', '/foo/bar.txt' but
+    // these will not '/foo/bar/baz', '/foo/bar/baz.txt'
+    // Lastely, when globstar is _true_, '/foo/**' is equivelant to '/foo/*' when
+    // globstar is _false_
+    var globstar = false;
+
+    // If we are doing extended matching, this boolean is true when we are inside
+    // a group (eg {*.html,*.js}), and false otherwise.
+    var inGroup = false;
+
+    // RegExp flags (eg "i" ) to pass in to RegExp constructor.
+    var flags = "";
+
+    var c;
+    for (var i = 0, len = str.length; i < len; i++) {
+        c = str[i];
+
+        switch (c) {
+            case "/":
+            case "$":
+            case "^":
+            case "+":
+            case ".":
+            case "(":
+            case ")":
+            case "=":
+            case "!":
+            case "|":
+                reStr += "\\" + c;
+                break;
+
+            case "?":
+                if (extended) {
+                    reStr += ".";
+                    break;
+                }
+
+            case "[":
+            case "]":
+                if (extended) {
+                    reStr += c;
+                    break;
+                }
+
+            case "{":
+                if (extended) {
+                    inGroup = true;
+                    reStr += "(";
+                    break;
+                }
+
+            case "}":
+                if (extended) {
+                    inGroup = false;
+                    reStr += ")";
+                    break;
+                }
+
+            case ",":
+                if (inGroup) {
+                    reStr += "|";
+                    break;
+                }
+                reStr += "\\" + c;
+                break;
+
+            case "*":
+                // Move over all consecutive "*"'s.
+                // Also store the previous and next characters
+                var prevChar = str[i - 1];
+                var starCount = 1;
+                while (str[i + 1] === "*") {
+                    starCount++;
+                    i++;
+                }
+                var nextChar = str[i + 1];
+
+                if (!globstar) {
+                    // globstar is disabled, so treat any number of "*" as one
+                    reStr += ".*";
+                } else {
+                    // globstar is enabled, so determine if this is a globstar segment
+                    var isGlobstar = starCount > 1                      // multiple "*"'s
+                        && (prevChar === "/" || prevChar === undefined)   // from the start of the segment
+                        && (nextChar === "/" || nextChar === undefined)   // to the end of the segment
+
+                    if (isGlobstar) {
+                        // it's a globstar, so match zero or more path segments
+                        reStr += "((?:[^/]*(?:\/|$))*)";
+                        i++; // move over the "/"
+                    } else {
+                        // it's not a globstar, so only match one path segment
+                        reStr += "([^/]*)";
+                    }
+                }
+                break;
+
+            default:
+                reStr += c;
+        }
+    }
+
+    // When regexp 'g' flag is specified don't
+    // constrain the regular expression with ^ & $
+    if (!flags || !~flags.indexOf('g')) {
+        reStr = "^" + reStr + "$";
+    }
+
+    return new RegExp(reStr, flags);
+};
 const IDENTIFIER_SIZE = 7;
 const METADATA_BEGIN_SYMBOL = "/[";
 const METADATA_END_SYMBOL = "]/";
@@ -595,16 +730,21 @@ class VoilDoc {
         this.updateWatcher();
     }
 
-    async getFilesRecursive(rootUri: vscode.Uri, prefix: string = '', ignoredPatterns: string[] = [], currentSize: number = 0): Promise<[string, vscode.FileType][]> {
+    async getFilesRecursive(rootUri: vscode.Uri, prefix: string = '', ignoredPatterns: RegExp[] = [], currentSize: number = 0): Promise<[string, vscode.FileType][]> {
         let files = await vscode.workspace.fs.readDirectory(rootUri);
         let res: [string, vscode.FileType][] = [];
         let gitignoreFile = vscode.Uri.joinPath(rootUri, '.gitignore');
         if (await vscode.workspace.fs.stat(gitignoreFile).then(() => true, () => false)) {
             let gitignoreContent = (await vscode.workspace.fs.readFile(gitignoreFile)).toString();
-            ignoredPatterns = ignoredPatterns.concat(gitignoreContent.split('\n').filter((line) => line.trim().length > 0));
+            ignoredPatterns = ignoredPatterns.concat(
+                gitignoreContent.split('\n')
+                .filter((line) => line.trim().length > 0)
+                .filter((line) => !line.startsWith("#"))
+                .map((line) => globToRegex(line.trim()))
+            );
         }
         for (let [name, type] of files) {
-            if (ignoredPatterns.some((pattern) => name.includes(pattern))) {
+            if (ignoredPatterns.some((pattern) => pattern.test(prefix + name))) {
                 continue;
             }
             if (type === vscode.FileType.Directory) {
@@ -1290,6 +1430,17 @@ export function activate(context: vscode.ExtensionContext) {
         }
 
         cutIdentifiers.clear();
+    });
+
+    const handleCd = vscode.commands.registerCommand('voil.cd', async () => {
+        let voil = await getVoilDocForActiveEditor();
+        if (voil){
+            let dir = await vscode.window.showInputBox({ prompt: 'Enter directory to go to' });
+            if (dir){
+                voil.currentDir = vscode.Uri.parse('/' + dir);
+                await updateDocContentToCurrentDir(voil);
+            }
+        }
     });
 
     const handleClose = vscode.commands.registerCommand('voil.close', async () => {
